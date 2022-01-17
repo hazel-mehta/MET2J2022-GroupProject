cca5b66c5a82de416908418cf879d7f20d1527a9
Elisa Corcoran <elisa.corcoran@gmail.com>
2022-01-17 14:47:56 +0100

    Trial filtering data for all athletes

--- trial.py
+++ trial.py
@@ -1,4 +1,10 @@
-with open('People/A_people.json', 'r') as file:
-    text = file.read()
+import json
+with open('People/A_people.json') as file:
+    text = json.load(file)
 
-print(text)
+    athletes = []
+    for person in text:
+        occupation_athlete = (person['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label'])
+        if 'athlete' in occupation_athlete:
+            athletes.append(person)
+print(athletes)
