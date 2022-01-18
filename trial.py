dc68a3241f563c09d70eb6158cc924e124f0423c
Prerana <2002preru@gmail.com>
2022-01-18 14:15:08 +0100

    converted list of dictionaries to .csv file

--- trial.py
+++ trial.py
@@ -1,10 +1,34 @@
 import json
-with open('People/A_people.json') as file:
-    text = json.load(file)
+alphabet = ['A']#, 'B', 'C', 'D']
+athletes = []
+for x in alphabet:
+    with open(f'People/{x}_people.json') as file:
+        text = json.load(file)
+        for person in text:
+            occupation_athlete = (person['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label'])
+            if 'basketball player' in occupation_athlete:
+                athletes.append(person)
 
-    athletes = []
-    for person in text:
-        occupation_athlete = (person['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label'])
-        if 'athlete' in occupation_athlete:
-            athletes.append(person)
-print(athletes)
+with open('athletes1.json','w') as file:
+    json.dump(athletes, file)
+
+
+gender = "female"
+for person in athletes:
+    if 'ontology/college_label' in person.keys():
+        for element in person['ontology/college_label']: 
+            if 'women' in element or 'female' in element:
+                gender = "female"
+    elif 'ontology/nationalTeam_label' in person.keys():
+        for element in person['ontology/nationalTeam_label']: 
+            if 'women' in element or 'female' in element:
+                gender = "female"
+    elif 'ontology/team_label' in person.keys():
+        for element in person['ontology/team_label']: 
+            if 'women' in element or 'female' in element:
+                gender = "female"
+    else: 
+        gender = "male"
+    
+    if gender == 'female':
+        print(gender)
