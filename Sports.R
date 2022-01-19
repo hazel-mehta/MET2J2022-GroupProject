f5364429e0e4b70ba27281340658e3781d184195
Elisa Corcoran <elisa.corcoran@gmail.com>
2022-01-19 15:54:15 +0100

    Plotting proportion of gender per sport

--- Sports.R
+++ Sports.R
@@ -1,11 +1,26 @@
 library('tidyverse')
 
-athletes<- read_csv('athletes_summary.csv')
-total_athletes = nrow(athletes) 
-summarized_athletes <- group_by(athletes, Gender, Sport)%>%
-  summarize(proportion = (n()/total_athletes)*100)
-  
-ggplot(data = summarized_athletes) +
-  aes(x = Sport, y = proportion, fill = Gender) +
-  geom_col()
+athletes <- read_csv('athletes_summary.csv')
+total_athletes = nrow(athletes)
+
+percentage_gender_total <- group_by(athletes, Gender)%>%
+  summarize(proportion = (n()/total_athletes)*100) 
+
+percentage_gender_sport <- group_by(athletes, Sport)%>%
+  mutate(sport_total = n()) %>%
+  ungroup()%>%
+  group_by(Gender, Sport) %>%
+  summarize(proportion_sport = (n()/mean(sport_total))*100) %>%
+  filter(
+          (Sport == 'soccer player' |
+           Sport == 'basketball player' |
+           Sport == 'golf player'| 
+           Sport == 'volleyball player') &
+           (Gender == 'female'|
+              Gender == 'male')
+          )
+
+ggplot(data = percentage_gender_sport) +
+  aes(x = Gender, y = proportion_sport, fill = Sport) +
+  geom_col(position = 'dodge')
   
