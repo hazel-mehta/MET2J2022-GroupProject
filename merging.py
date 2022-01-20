4211907788e7dcee24286a66f3272c49490964c9
Elisa Corcoran <elisa.corcoran@gmail.com>
2022-01-20 11:54:08 +0100

    cleaned code with merged data

--- merging.py
+++ merging.py
@@ -8,7 +8,6 @@ with open ('wiki_genders.txt') as brandon_file:
         wiki_id, gender, title = line.strip().split('\t')
         gender_information.update({title:gender})
 
-# print(gender_information)
 
 with open('athletes_dictionary.json') as file:
     athletes_text = json.load(file)
@@ -16,11 +15,10 @@ with open('athletes_dictionary.json') as file:
         person_clean = person['Name'].replace('_', ' ')
         if person_clean in gender_information.keys():
             person['Replaced_Gender'] = gender_information[person_clean]
-            #print(f"Replaced {person_clean}'s gender!")
+            
         else:
             person['Replaced_Gender'] = 'unspecified'
-        # if name[2] in person['Name']:
-        #     person["Gender"] = name[1]
+    
         summary_athletes_gender.append(person)
         
 
@@ -43,5 +41,3 @@ with open("comparative_gender_athletes.csv", "w", newline = "", encoding = 'utf8
     dict_writer = csv.DictWriter(file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(summary_athletes_gender)
-
-    
